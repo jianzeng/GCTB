ab249f8de241d3e09103357d5a936c10484abc6f
Jian Zeng <jzengr@gmail.com>
2017-12-05 19:01:06 +1000

    output mean and variance of LD matrix diagonals.

--- scr/data.cpp
+++ scr/data.cpp
@@ -1161,7 +1161,7 @@ void Data::makeLDmatrix(const string &bedFile, const unsigned windowWidth, const
     timer.getTime();
     
     cout << "Average window size " << windSize.sum()/numIncdSnps << "." << endl;
-    cout << "LD matrix diagonal mean " << ZPZdiag.mean() << " variance " << ZPZdiag.squaredNorm()/numIncdSnps << "." << endl;
+    cout << "LD matrix diagonal mean " << ZPZdiag.mean() << " variance " << (ZPZdiag.array()-ZPZdiag.mean()).square().sum()/numIncdSnps << "." << endl;
     cout << "Genotype data for " << numKeptInds << " individuals and " << numIncdSnps << " SNPs are included from [" + bedFile + "]." << endl;
     cout << "Build of LD matrix completed (time used: " << timer.format(timer.getElapse()) << ")." << endl;
     
