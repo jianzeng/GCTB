ed4c8047b8a5ce077c072b9ee504f287fbe00fda
Jian Zeng <jzengr@gmail.com>
2017-12-08 22:59:41 +1000

    address integer overflow in computing the mean of window size.

--- scr/data.cpp
+++ scr/data.cpp
@@ -1421,7 +1421,7 @@ void Data::makeLDmatrix(const string &bedFile, const float LDthreshold, const st
     timer.getTime();
     
     
-    cout << "Window size mean " << windSize.mean() << " sd " << unsigned(sqrt(Gadget::calcVariance(windSize.cast<float>()))) << "." << endl;
+    cout << "Window size mean " << windSize.sum()/numIncdSnps << " sd " << unsigned(sqrt((windSize.array()-windSize.sum()/numIncdSnps).square().sum()/numIncdSnps)) << "." << endl;
     cout << "LD matrix diagonal mean " << ZPZdiag.mean() << " variance " << Gadget::calcVariance(ZPZdiag) << "." << endl;
     cout << "Genotype data for " << numKeptInds << " individuals and " << numSnpInRange << " SNPs are included from [" + bedFile + "]." << endl;
     cout << "Build of LD matrix completed (time used: " << timer.format(timer.getElapse()) << ")." << endl;
@@ -1580,7 +1580,7 @@ void Data::readLDmatrixBinFile(const string &ldmatrixFile){
     timer.getTime();
     
 //    cout << "Window width " << windowWidth << " Mb." << endl;
-    cout << "Window size mean " << windSize.mean() << " sd " << unsigned(sqrt(Gadget::calcVariance(windSize.cast<float>()))) << "." << endl;
+    cout << "Window size mean " << windSize.sum()/numIncdSnps << " sd " << unsigned(sqrt((windSize.array()-windSize.sum()/numIncdSnps).square().sum()/numIncdSnps)) << "." << endl;
     cout << "LD matrix diagnal mean " << ZPZdiag.mean() << " sd " << sqrt(Gadget::calcVariance(ZPZdiag)) << "." << endl;
     cout << "Read LD matrix for " << numIncdSnps << " SNPs (time used: " << timer.format(timer.getElapse()) << ")." << endl;
 }
@@ -1750,7 +1750,7 @@ void Data::readMultiLDmatBinFile(const string &mldmatFile){
     
     timer.getTime();
     
-    cout << "Window size mean " << windSize.mean() << " sd " << unsigned(sqrt(Gadget::calcVariance(windSize.cast<float>()))) << "." << endl;
+    cout << "Window size mean " << windSize.sum()/numIncdSnps << " sd " << unsigned(sqrt((windSize.array()-windSize.sum()/numIncdSnps).square().sum()/numIncdSnps)) << "." << endl;
     cout << "LD matrix diagnal mean " << ZPZdiag.mean() << " sd " << sqrt(Gadget::calcVariance(ZPZdiag)) << "." << endl;
     cout << "Read LD matrix for " << numIncdSnps << " SNPs (time used: " << timer.format(timer.getElapse()) << ")." << endl;
 }
