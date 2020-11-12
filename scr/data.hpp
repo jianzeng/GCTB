2009ad8ef891952e1b85cbe858d25b12e2e6c501
ZhiliZheng <zhilizheng@outlook.com>
2020-11-12 10:49:09 +1000

    tidy

--- scr/data.hpp
+++ scr/data.hpp
@@ -8,6 +8,9 @@
 
 #ifndef data_hpp
 #define data_hpp
+#include <complex>
+#define lapack_complex_float std::complex<float>
+#define lapack_complex_double std::complex<double>
 
 #include <iostream>
 #include <stdio.h>
